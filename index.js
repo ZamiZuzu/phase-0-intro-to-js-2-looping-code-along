c4e23ed63dace6d044087002440559b5968c1f2b
Christopher Valle <chrisbv@live.com>
2021-10-01 00:15:44 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+let card = [];
+
+function writeCards(names, event){
+    for (let i = 0; i < names.length; i++) {
+        card.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+return card;
+}
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown);
+        countDown--;
+    }
+}
